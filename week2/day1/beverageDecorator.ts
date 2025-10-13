cbc43e3c80800f878738f6b32e10eae0699a41bb
umadevi-12 <bogathiu@gmail.com>
2025-10-13 22:24:46 +0530

    done

---
+++ week2/day1/beverageDecorator.ts
@@ -0,0 +1,38 @@
+
+abstract class Beverage {
+    abstract getDescription(): string;
+    abstract getCost(): number;
+}
+
+class GreenTea extends Beverage {
+    getDescription(): string {
+        return "Green Tea";
+    }
+
+    getCost(): number {
+        return 40;
+    }
+}
+
+abstract class BeverageDecorator extends Beverage {
+    protected beverage: Beverage;
+
+    constructor(beverage: Beverage) {
+        super();
+        this.beverage = beverage;
+    }
+}
+
+class Sugar extends BeverageDecorator {
+    getDescription(): string {
+        return this.beverage.getDescription() + " + Sugar";
+    }
+
+    getCost(): number {
+        return this.beverage.getCost() + 10;
+    }
+}
+
+const teaWithSugar = new Sugar(new GreenTea());
+console.log(teaWithSugar.getDescription()); 
+console.log(teaWithSugar.getCost());        
