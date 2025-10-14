94fed04b418749ac3d8eda0d5288d4e97973f274
umadevi-12 <bogathiu@gmail.com>
2025-10-14 22:39:59 +0530

    done

---
+++ week3/day5/bookFactory.js
@@ -0,0 +1,45 @@
+
+class Book {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  getCategory() {
+    throw new Error("getCategory() must be implemented by subclass");
+  }
+}
+class PremiumBook extends Book {
+  constructor(title, price) {
+    super(title, price);
+  }
+
+  getCategory() {
+    return "Premium Book";
+  }
+}
+
+class RegularBook extends Book {
+  constructor(title, price) {
+    super(title, price);
+  }
+
+  getCategory() {
+    return "Regular Book";
+  }
+}
+class BookFactory {
+  static createBook(title, price) {
+    if (price > 1000) {
+      return new PremiumBook(title, price);
+    } else {
+      return new RegularBook(title, price);
+    }
+  }
+}
+
+const b1 = BookFactory.createBook("Design Patterns", 1500);
+console.log(b1.getCategory()); 
+
+const b2 = BookFactory.createBook("JavaScript Guide", 500);
+console.log(b2.getCategory()); 
